6c5b37ae0f4cdf2ad83b41b95b3f1b0762587e3e
azaliyaraisovna <azaliyaraisovna@gmail.com>
2021-02-06 11:09:49 +0300

    add houses and graves

---
+++ NJ/Entities/Actors/Sceneries/Grave.cs
@@ -0,0 +1,39 @@
+﻿using KTEngine;
+using Microsoft.Xna.Framework;
+using System.Xml;
+
+namespace Chip
+{
+    public class Grave : Actor
+    {
+        private Animation _sprite;
+        private int _type = 0;
+        private float amp;
+        private int fade;
+
+        public Grave()
+            : base(0, 0)
+        {
+            Depth = 2;
+
+            MoveCollider = Add(new Hitbox(-2, -4, 4, 8));
+            //MoveCollider.Tag((int)Tags.HiddenPlace);
+        }
+
+        public override void CreateFromXml(XmlElement xml, Room room, Vector2 offset, Level level)
+        {
+            base.CreateFromXml(xml, room, offset, level);
+            _type = xml.AttrInt("type");
+
+            X = offset.X;
+            Y = offset.Y + 2;
+
+            _sprite = new Animation(GFX.Game["sceneries/grave" + _type], 4, 8);
+            Add(_sprite);
+            _sprite.Add("idle", 0f, 0);
+            _sprite.Origin.X = 2f;
+            _sprite.Origin.Y = 4f;
+            _sprite.Play("idle");
+        }
+    }
+}
---
+++ NJ/Entities/Actors/Sceneries/House.cs
@@ -0,0 +1,39 @@
+﻿using KTEngine;
+using Microsoft.Xna.Framework;
+using System.Xml;
+
+namespace Chip
+{
+    public class House : Actor
+    {
+        private Animation _sprite;
+        private int _type = 0;
+        private float amp;
+        private int fade;
+
+        public House()
+            : base(0, 0)
+        {
+            Depth = 2;
+
+            MoveCollider = Add(new Hitbox(-4, -4, 8, 8));
+            //MoveCollider.Tag((int)Tags.HiddenPlace);
+        }
+
+        public override void CreateFromXml(XmlElement xml, Room room, Vector2 offset, Level level)
+        {
+            base.CreateFromXml(xml, room, offset, level);
+            _type = xml.AttrInt("type");
+
+            X = offset.X;
+            Y = offset.Y + 2;
+
+            _sprite = new Animation(GFX.Game["sceneries/house" + _type], 8, 8);
+            Add(_sprite);
+            _sprite.Add("idle", 0f, 0);
+            _sprite.Origin.X = 4f;
+            _sprite.Origin.Y = 4f;
+            _sprite.Play("idle");
+        }
+    }
+}
